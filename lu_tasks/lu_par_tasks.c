869543ca749d5c48175cdf960ab76eb6af3c553b
Wilfried Bounsi <wilcoln99@gmail.com>
2020-02-04 05:50:40 +0100

    init

---
+++ lu_tasks/lu_par_tasks.c
@@ -0,0 +1,42 @@
+#include "trace.h"
+#include "common.h"
+#include <omp.h>
+
+/* This is a sequential routine for the LU factorization of a square
+   matrix in block-columns */
+void lu_par_tasks(Matrix A, info_type info){
+
+
+  int i, j;
+
+  trace_init();
+  #pragma omp parallel
+  {
+    for(i=0; i<info.NB; i++){
+      /* Do the panel */
+      #pragma omp task depend(out:A[i])
+      {
+        panel(A[i], i, info);
+      }
+      
+      #pragma omp for
+      for(j=i+1; j<info.NB; j++){
+        /* Do all the correspondint updates */
+        #pragma omp task depend(in:A[i], inout:A[j])
+        {
+          update(A[i], A[j], i, j, info);
+        }
+      }
+    }
+  }
+  
+  /* Do row permutations resulting from the numerical pivoting */
+  /* This operation can be ignored and should be left out of the parallel region */
+  backperm(A, info);
+
+  trace_dump("trace_par_tasks.svg");
+
+  return;
+
+}
+
